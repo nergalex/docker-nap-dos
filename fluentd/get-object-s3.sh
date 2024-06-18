e9bf21cbded1e9935ffd6afa8374b752a2c29125
nergalex <nergalex@hotmail.com>
2024-06-18 14:12:44 +0200

    tmp

--- fluentd/get-object-s3.sh
+++ fluentd/get-object-s3.sh
@@ -22,7 +22,7 @@ DATE=$(date -u +"%Y%m%d")
 EMPTY_STRING_HASH=$(printf "" | openssl dgst -sha256 | cut -d ' ' -f 2)
 
 #Store Canonical request
-/bin/cat >./canonical_request.tmp <<EOF
+/bin/cat >/tmp-test/canonical_request.tmp <<EOF
 $HTTP_METHOD
 $CANONICAL_URI
 
@@ -36,10 +36,10 @@ $EMPTY_STRING_HASH
 EOF
 
 # Remove trailing newline
-printf %s "$(cat canonical_request.tmp)" > canonical_request.tmp
+printf %s "$(cat /tmp-test/canonical_request.tmp)" > /tmp-test/canonical_request.tmp
 
 # Generate canonical request hash
-CANONICAL_REQUEST_HASH=$(openssl dgst -sha256 canonical_request.tmp | awk -F ' ' '{print $2}')
+CANONICAL_REQUEST_HASH=$(openssl dgst -sha256 /tmp-test/canonical_request.tmp | awk -F ' ' '{print $2}')
 
 # Function to generate sha256 hash
 function hmac_sha256 {
@@ -55,20 +55,20 @@ DATE_REGION_SERVICE_KEY=$(hmac_sha256 hexkey:$DATE_REGION_KEY $AWS_SERVICE)
 HEX_KEY=$(hmac_sha256 hexkey:$DATE_REGION_SERVICE_KEY "aws4_request")
 
 # Store string to sign
-/bin/cat >./string_to_sign.tmp <<EOF
+/bin/cat >/tmp-test/string_to_sign.tmp <<EOF
 AWS4-HMAC-SHA256
 $DATE_AND_TIME
 $DATE/$REGION/$AWS_SERVICE/aws4_request
 $CANONICAL_REQUEST_HASH
 EOF
 
-printf %s "$(cat string_to_sign.tmp)" > string_to_sign.tmp
+printf %s "$(cat /tmp-test/string_to_sign.tmp)" > /tmp-test/string_to_sign.tmp
 
 # Generate signature
-SIGNATURE=$(openssl dgst -sha256 -mac HMAC -macopt hexkey:$HEX_KEY string_to_sign.tmp | awk -F ' ' '{print $2}')
+SIGNATURE=$(openssl dgst -sha256 -mac HMAC -macopt hexkey:$HEX_KEY /tmp-test/string_to_sign.tmp | awk -F ' ' '{print $2}')
 
 # Remove temporary files
-rm canonical_request.tmp string_to_sign.tmp
+rm /tmp-test/canonical_request.tmp /tmp-test/string_to_sign.tmp
 
 # Remove file prefix
 OUTPUT=$(echo $2 |  awk -F '/' '{print $NF}')
